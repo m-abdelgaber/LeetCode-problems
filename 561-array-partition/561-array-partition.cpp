3d7e893c573f8c309b7bf31ebefc9d0cc192eef9
Mohamed Mahmoud Abdulatif <konar.mohamed@gmail.com>
2022-07-20 10:46:44 +0200

    Time: 55 ms (94.04%), Space: 28.3 MB (48.02%) - LeetHub

--- 561-array-partition/561-array-partition.cpp
+++ 561-array-partition/561-array-partition.cpp
@@ -4,7 +4,7 @@ public:
         int res=0;
         sort(nums.begin(), nums.end());
         for(int i=0;i < nums.size(); i = i +2){
-            res+= min(nums[i], nums[i+1]);
+            res+= nums[i];
         }
         return res;
     }
