7d2cfe7d922e01c9d6c8afaa92e8115df96b2bae
Mohamed Mahmoud Abdulatif <konar.mohamed@gmail.com>
2022-07-22 13:05:48 +0200

    Time: 24 ms, Memory: 9.7 MB - LeetHub

--- reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
+++ reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
@@ -1,33 +1,42 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        string res ="";
-        int fP=0; 
-        int sP=0;
+//         string res ="";
+//         int fP=0; 
+//         int sP=0;
+//         int n = s.size();
         
-        
-        while(fP < s.size() && sP < s.size()){
-            for(;sP<s.size(); sP++){
-                if(s[sP] == ' '){
+//         while(fP < n && sP < n){
+//             for(;sP<n; sP++){
+//                 if(s[sP] == ' '){
                     
-                    break;
-                }
-            }
-            string subs="";
-            for(int i = fP; i<sP; i++){
-                subs = s[i]+ subs;
-            }
+//                     break;
+//                 }
+//             }
+//             string subs="";
+//             for(int i = fP; i<sP; i++){
+//                 subs = s[i]+ subs;
+//             }
             
-            res = res + subs+ " ";
-            fP = ++sP;
-            for(; fP< s.size(); fP++){
-                if(s[fP] != ' '){
-                    sP = fP;
-                    break;
-                }
-             }
+//             res = res + subs+ " ";
+//             fP = ++sP;
+//             for(; fP< n; fP++){
+//                 if(s[fP] != ' '){
+//                     sP = fP;
+//                     break;
+//                 }
+//              }
+//         }
+//         return res.substr(0, n);   
+        
+        int fP;
+        for(int sP=0; sP<s.size(); sP++){
+            if(s[sP] == ' '){
+                reverse(s.begin() + fP, s.begin() + sP);
+                fP = sP +1;
+            }
         }
-        cout<<fP<<" "<< sP;
-        return res.substr(0, res.size()-1);   
+        reverse(s.begin() + fP, s.end());
+        return s;
     }
 };
