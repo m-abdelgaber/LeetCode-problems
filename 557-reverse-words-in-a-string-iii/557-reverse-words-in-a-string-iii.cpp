c1055580134c4b384c7e41a074b5edaceb85665f
Mohamed Mahmoud Abdulatif <konar.mohamed@gmail.com>
2022-07-22 11:51:00 +0200

    Time: 283 ms (5.02%), Space: 294.6 MB (5.05%) - LeetHub

---
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string res ="";
+        int fP=0; 
+        int sP=0;
+        
+        
+        while(fP < s.size() && sP < s.size()){
+            for(;sP<s.size(); sP++){
+                if(s[sP] == ' '){
+                    
+                    break;
+                }
+            }
+            string subs="";
+            for(int i = fP; i<sP; i++){
+                subs = s[i]+ subs;
+            }
+            
+            res = res + subs+ " ";
+            fP = ++sP;
+            for(; fP< s.size(); fP++){
+                if(s[fP] != ' '){
+                    sP = fP;
+                    break;
+                }
+             }
+        }
+        return res.substr(0, res.size()-1);   
+    }
+};
