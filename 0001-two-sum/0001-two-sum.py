9e789b2e14cc331d58de2a0d3bc20156486b0089
Mohamed Abdelgaber <konar.mohamed@gmail.com>
2023-01-02 15:32:49 +0200

    Time: 2640 ms (32.89%), Space: 14.4 MB (48.94%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        
+        
+        for i in range(0,len(nums)-1):
+            for j in range(i+1, len(nums)):
+                summation = nums[i]+ nums[j]
+                if(summation == target):
+                    return i,j
