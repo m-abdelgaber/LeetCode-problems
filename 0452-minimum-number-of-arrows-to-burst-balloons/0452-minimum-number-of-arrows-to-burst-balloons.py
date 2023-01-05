4d4a8b0766a54d90f26d9526ed87729c046dd6b9
Mohamed Abdelgaber <konar.mohamed@gmail.com>
2023-01-05 04:06:40 +0200

    Time: 4839 ms (5.33%), Space: 63.2 MB (15.38%) - LeetHub

---
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def findMinArrowShots(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        # sorting the points by the x-Start of them
+        def compare(item1, item2):
+            if item1[0] < item2[0]:
+                return -1
+            elif item1[0] > item2[0]:
+                return 1
+            elif item1[0] == item2[0] and item1[1] < item2[1]:
+                return -1
+            elif item1[0] == item2[0] and item1[1] > item2[1]:
+                return 1
+            else:
+                return 0
+        def get_intersect(item1, item2):
+            max_left = max(item1[0], item2[0])
+            min_right = min(item1[1], item2[1])
+            if max_left> min_right:
+                return -1
+            return [max_left, min_right]
+        ponts2 = sorted(points, cmp=compare)
+        # print(points)
+        # print(sorted(points, cmp=compare))
+        count = 0
+        curr_range = ponts2[0]
+        for item in ponts2:
+            intersect = get_intersect(curr_range, item)
+            if(intersect ==-1):
+                count+=1
+                curr_range = item
+            else:
+                curr_range = intersect
+        count+=1
+        return count
+        
