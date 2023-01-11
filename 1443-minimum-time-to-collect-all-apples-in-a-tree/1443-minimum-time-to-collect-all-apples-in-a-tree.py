31ee6b99e197155ac59776ca3f2f5ccedc100af1
Mohamed Abdelgaber <konar.mohamed@gmail.com>
2023-01-11 08:14:24 +0200

    Time: 595 ms (77.78%), Space: 60 MB (22.22%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
@@ -9,7 +9,7 @@ class Solution(object):
         if (n ==1):
             return 0;
         dic_edges = {}
-        visited_nodes = set()
+        visited_nodes =set()
         for edge in edges:
             if edge[0] in dic_edges:
                 dic_edges[edge[0]].append(edge[1])
@@ -21,17 +21,16 @@ class Solution(object):
                 dic_edges[edge[1]] = [edge[0]]
 
         def recursive_traversal( node_num):
-            
-            if (node_num in visited_nodes):
-                return 0
             visited_nodes.add(node_num)
+            # if (node_num in visited_nodes):
+            #     return 0
             # if node_num is not out of binary edges bounds (might have children nodes)
             if(node_num in dic_edges):
                 # if the node with node_num has edges with (children)  
                 edges_traversed =0
                 for child_node_num in dic_edges[node_num]:
-                    # if(child_node_num not in visited_nodes):
-                    edges_traversed += recursive_traversal( child_node_num)
+                    if(child_node_num not in visited_nodes):
+                        edges_traversed += recursive_traversal( child_node_num)
                     
                 if(edges_traversed>0):
                     # children traversed edges
