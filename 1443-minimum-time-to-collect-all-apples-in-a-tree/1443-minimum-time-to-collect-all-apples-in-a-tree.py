e5ee3b64095b2b6158e4690c2944bf5e74b350d5
Mohamed Abdelgaber <konar.mohamed@gmail.com>
2023-01-11 08:12:43 +0200

    Time: 615 ms (72.22%), Space: 59.9 MB (22.22%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
@@ -6,22 +6,43 @@ class Solution(object):
         :type hasApple: List[bool]
         :rtype: int
         """
-        adj = [[] for _ in range(n)]
-        for u, v in edges:
-            adj[u].append(v)
-            adj[v].append(u)
+        if (n ==1):
+            return 0;
+        dic_edges = {}
+        visited_nodes = set()
+        for edge in edges:
+            if edge[0] in dic_edges:
+                dic_edges[edge[0]].append(edge[1])
+            else:
+                dic_edges[edge[0]] = [edge[1]]
+            if edge[1] in dic_edges:
+                dic_edges[edge[1]].append(edge[0])
+            else:
+                dic_edges[edge[1]] = [edge[0]]
 
-        visited = set()
-        def dfs(node):
-            if node in visited:
+        def recursive_traversal( node_num):
+            
+            if (node_num in visited_nodes):
                 return 0
-            visited.add(node)
-            secs = 0
-            for child in adj[node]:
-                secs += dfs(child)
-            if secs > 0:
-                return secs + 2
-            return 2 if hasApple[node] else 0
-
-        return max(dfs(0) - 2, 0)
-                
+            visited_nodes.add(node_num)
+            # if node_num is not out of binary edges bounds (might have children nodes)
+            if(node_num in dic_edges):
+                # if the node with node_num has edges with (children)  
+                edges_traversed =0
+                for child_node_num in dic_edges[node_num]:
+                    # if(child_node_num not in visited_nodes):
+                    edges_traversed += recursive_traversal( child_node_num)
+                    
+                if(edges_traversed>0):
+                    # children traversed edges
+                    return 1 + edges_traversed
+            # no edges with children, but has apple
+            if(hasApple[node_num]):
+                return 1
+            # no edges with children and no apples
+            return 0
+        
+        res = recursive_traversal(0) -1 
+        if(res<0):
+            return 0
+        return res*2
