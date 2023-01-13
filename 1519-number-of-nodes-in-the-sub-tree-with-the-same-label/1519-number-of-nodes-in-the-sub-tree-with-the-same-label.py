c43b496729d8483c8f380f43355eb782717e6559
Mohamed Abdelgaber <konar.mohamed@gmail.com>
2023-01-13 09:18:50 +0200

    Time: 4036 ms (57.14%), Space: 186.6 MB (14.29%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.py
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.py
@@ -6,58 +6,35 @@ class Solution(object):
         :type labels: str
         :rtype: List[int]
         """
-#         if n==1:
-#             return n
+        # this creates a defaultdict with the default factory set to list to be able
+        # to access indicies directly without initialize and return [] if not available
         
+        g= defaultdict(list)
+        ans = [0]*n
+        visited = set()
         
-#         # print(node_similar_child_count)
+        for a,b in edges:
+            g[a]+= [b]
+            g[b]+= [a]
         
-        
-#         adjacecy_list = [[] for i in range(n)]
-#         # print(graph)
-#         for idx in range(0, len(edges)):
-            
-#             adjacecy_list[edges[idx][0]].append(edges[idx][1])
-            
-#             adjacecy_list[edges[idx][1]].append(edges[idx][0])
-            
-#         visited = set()
-#         queue =  [0]
-#         label_sub_group = {}
-#         while(len(queue)>0):
-#             elem = queue.pop()
-#             adjacent_group = adjacecy_list[elem]
-#             if labels[elem] in label_sub_group:
-#                 print(adjacecy_list)
-#                 # print(label_sub_group)
-#                 for group_elem in label_sub_group[labels[elem]]:
-#                     # print(group_elem[1])
-#                     if(group_elem not in adjacent_group ):
-#                         group_elem[1] +=1
-#             else:
-#                 label_sub_group[labels[elem]] = [[elem, 1]]
-#             visited.add(elem)
-            
-#             for adj_elem in adjacent_group:
-#                 if adj_elem in visited:
-#                     continue
-#                 queue.append(adj_elem)
         def dfs(node):
+            # A counter object is used to count the number of occurrences of items in a
+            # list or other iterable. It takes an iterable as input, and it creates a
+            # dictionary-like object where the keys are elements from the iterable and the
+            # values are their frequency count.
             cnt = Counter()
-            if node not in seen:
-                cnt[labels[node]] += 1 
-                seen.add(node)
-                for child in g.get(node, []):
+            if node not in visited:
+                visited.add(node)
+                cnt[labels[node]] +=1
+                # return [] if key doesn't exist
+                children = g.get(node, [])
+                for child in children:
                     cnt += dfs(child)
                 ans[node] = cnt[labels[node]]
             return cnt
-        g, ans, seen = defaultdict(list), [0] * n, set()
-        for a, b in edges:
-            g[a] += [b]
-            g[b] += [a]
         dfs(0)
-        return ans 
+        return ans
     
         
     
-        # print(label_sub_group)
+        
