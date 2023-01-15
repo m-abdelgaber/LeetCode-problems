70cf897951fba992a983284300a0c343cd5f09a4
Mohamed Abdelgaber <konar.mohamed@gmail.com>
2023-01-16 01:15:05 +0200

    Time: 2897 ms (84.62%), Space: 46 MB (34.61%) - LeetHub

---
+++ 2421-number-of-good-paths/2421-number-of-good-paths.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def numberOfGoodPaths(self, vals, edges):
+        """
+        :type vals: List[int]
+        :type edges: List[List[int]]
+        :rtype: int
+        """
+        res = n = len(vals)
+        f = list(range(n))
+        count = [Counter({vals[i]: 1}) for i in range(n)]
+        edges = sorted([max(vals[i], vals[j]),i,j] for i,j in edges)
+
+        def find(x):
+            if f[x] != x:
+                f[x] = find(f[x])
+            return f[x]
+
+        for v,i,j in edges:
+            fi, fj = find(i), find(j)
+            cj, ci = count[fi][v], count[fj][v]
+            res += ci * cj
+            f[fj] = fi
+            count[fi] = Counter({v: ci + cj})
+        return res
