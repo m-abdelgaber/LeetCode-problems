5a39a0878ef0e8d197a1bec7098cfa41b38e0173
Mohamed Abdelgaber <konar.mohamed@gmail.com>
2023-01-28 12:19:26 +0200

    Time: 51 ms (75.33%), Space: 14.4 MB (63.24%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -11,10 +11,12 @@ class Solution:
                 s_chars_count[char] =1
         
         
-        unmatched_chars_counter = len(s)
         for t_char in t:
             if t_char not in s_chars_count or s_chars_count[t_char] ==0:
                 return False
             s_chars_count[t_char] -=1
-            unmatched_chars_counter-=1
-        return unmatched_chars_counter ==0
+        
+        for key in s_chars_count:
+            if s_chars_count[key] <0:
+                return False
+        return True
